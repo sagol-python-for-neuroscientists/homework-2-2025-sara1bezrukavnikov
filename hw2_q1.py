ba17598ebabfaad7ab6df0061e01df5d5289c2e0
Sara Bezrukavnikov <sara.bezrukavnikov@gmail.com>
2025-04-21 18:15:12 +0300

    Fixed the Git repo

--- hw2_q1.py
+++ hw2_q1.py
@@ -1,3 +1,6 @@
+import re
+from pathlib import Path
+
 MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
               'D': '-..',    'E': '.',      'F': '..-.',
               'G': '--.',    'H': '....',   'I': '..',
@@ -16,7 +19,6 @@ MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
               '.': '.-.-.-', ',': '--..--', ':': '---...',
               "'": '.----.', '-': '-....-',
               }
-
 
 def english_to_morse(
     input_file: str = "lorem.txt",
@@ -37,3 +39,19 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+
+    script_location = Path(__file__).parent.resolve()  # full path to this script
+    input_path = script_location / input_file
+    output_path = script_location / output_file
+
+    with open(input_path, 'r') as f:
+        lorem_str = f.read().upper() # Converting everything to capital since our dictionary is for uppercase characters
+
+    new_str = re.sub(r'[A-Z0-9\.\,\:\'\-]', lambda m: MORSE_CODE[m.group()], lorem_str) # avoiding a loop with a lambda
+    new_str = new_str.replace(' ', '\n')
+    
+    with open(output_path, 'w') as f:
+        f.write(new_str)
+
+if __name__ == "__main__":
+    english_to_morse()
--- hw2_q2.py
+++ hw2_q2.py
@@ -1,9 +1,9 @@
-from collections import namedtuple
 from enum import Enum
+from collections import namedtuple
+
 
 Condition = Enum("Condition", ("CURE", "HEALTHY", "SICK", "DYING", "DEAD"))
 Agent = namedtuple("Agent", ("name", "category"))
-
 
 def meetup(agent_listing: tuple) -> list:
     """Model the outcome of the meetings of pairs of agents.
@@ -19,13 +19,83 @@ def meetup(agent_listing: tuple) -> list:
     Parameters
     ----------
     agent_listing : tuple of Agent
-        A listing (tuple in this case) in which each element is of the Agent
-        type, containing a 'name' field and a 'category' field, with 'category' being
-        of the type Condition.
+        A listing (tuple in this case) in which each element is of the Agent type, containing a 'name' field and a 'category' field, with 'category' being of the type Condition.
 
     Returns
     -------
     updated_listing : list
-        A list of Agents with their 'category' field changed according to the result
-        of the meeting.
+        A list of Agents with their 'category' field changed according to the result of the meeting.
     """
+    
+    length = len(agent_listing)
+
+    if length == 0:
+        return []
+    
+    new_list = []
+
+    if length == 1:
+        new_list.extend(agent_listing)
+        return new_list
+    
+    iterable_list, non_iterable_list = filtering_out(agent_listing)
+
+    new_list = []
+
+    for i in range(0, len(iterable_list) - 1, 2):
+        a1, a2 = disease_spread(iterable_list[i], iterable_list[i+1])
+        new_list.extend([a1, a2])
+
+    if len(iterable_list) % 2 == 1:
+        new_list.append(iterable_list[-1])
+    
+    new_list.extend(non_iterable_list)
+
+    # print("\n".join(str(item) for item in new_list))
+
+    return(new_list)
+
+# Defining an axuliary function for better printing
+
+def filtering_out(agent_listing: tuple) -> list:
+
+    eligible_list = []
+    non_eligible_list = []
+
+    for agent in agent_listing:
+        if is_eligible(agent):
+            eligible_list.append(agent)
+        else:
+            non_eligible_list.append(agent)
+    
+    return eligible_list, non_eligible_list
+
+def is_eligible(agent1: Agent):
+    if agent1.category not in (Condition.HEALTHY, Condition.DEAD):
+        return True
+    else:
+        return False
+
+# Defining an auxiliary function with disease spreading logic
+
+def disease_spread(agent1: Agent, agent2: Agent) -> tuple:
+
+# Adding this because you can't modify the original enum
+    cat1 = agent1.category
+    cat2 = agent2.category
+
+    # CURE scenarios: if either one of them is of CURE status, bump Condition.SICK up to "Healthy", and Condition.DYING up to Condition.SICK by decreasing value by 1
+    if cat1 == Condition.CURE and cat2 in (Condition.SICK, Condition.DYING):
+        cat2 = Condition(cat2.value - 1)
+    if cat2 == Condition.CURE and cat1 in (Condition.SICK, Condition.DYING):
+        cat1 = Condition(cat1.value - 1)
+
+    # SICK/DYING meeting logic
+    if cat1 in (Condition.SICK, Condition.DYING) and cat2 in (Condition.SICK, Condition.DYING):
+        cat1 = Condition(cat1.value + 1)
+        cat2 = Condition(cat2.value + 1)
+
+    # SICK scenarios: SICK → DYING, DYING → DEAD; adding +1 because the original ENUM goes from healthy to dead
+    # accounting only for SICK & DYING conditions because all others are unaffected by meetings
+
+    return Agent(agent1.name, cat1), Agent(agent2.name, cat2)
